232b440e7e8441b7f0c6ebb9a1dcac8517454813
David Conner <dconner.pro@gmail.com>
2016-02-28 06:15:53 -0500

    started implementing muse listener

--- MuseStatsIos/MuseListener.swift
+++ MuseStatsIos/MuseListener.swift
@@ -10,18 +10,44 @@ import Foundation
 
 @objc class MuseListener: NSObject, IXNMuseDataListener, IXNMuseConnectionListener {
     
-    func receiveMuseConnectionPacket(packet: IXNMuseConnectionPacket!) {
-        
-        
-    }
+    weak var delegate: AnyObject? // ?? lulz
     
     func receiveMuseDataPacket(packet: IXNMuseDataPacket!) {
-        
-        
+        switch packet.packetType {
+        case .Battery: print("yo. battery's low."); break
+        case .Accelerometer: break
+        // case: alot of these
+        default: break
+        }
     }
     
     func receiveMuseArtifactPacket(packet: IXNMuseArtifactPacket!) {
+        guard packet.headbandOn else {
+            return
+        }
+
+        if packet.blink {
+            print("it's a blink!")
+        }
+    }
+    
+    func receiveMuseConnectionPacket(packet: IXNMuseConnectionPacket!) {
+        //holy shitty fuck!
+        var state: String = "who the fuck knows!"
+        switch packet.currentConnectionState {
+        case .Unknown: state = "Unknown"; break
+        case .NeedsUpdate: state = "Needs update"; break
+        case .Connecting: state = "Connecting..."; break
+        case .Connected: state = "Connected"; break
+        case .Disconnected: state = "Disconnected"; break
+        default: assert(false, "this shit is soooo fucked up and you know it"); break
+        }
         
+        print("Status: \(state)")
+        
+        if (packet.currentConnectionState == .Disconnected) {
+            self.delegate?.performSelector("reconnectToMuse", withObject: nil, afterDelay: 0)
+        }
         
         
     }
